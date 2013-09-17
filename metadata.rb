ef36921e97c1c99433bed146668ace1d3d3ed7e2
Erick Turnquist <eturnquist@visiblemeasures.com>
2013-09-17 11:08:24 -0400

    don't barf if there are no domains set

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'jhujhiti@adjectivism.org'
 license          'Public domain'
 description      'Configures PAM resource limits'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
